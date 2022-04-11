c5f6aa5fda4f796ea7fbed7f541b964aa83b89d4
selemonjr <selemonjr@gmail.com>
2022-04-11 00:54:42 -0700

    update

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-<div class="gridBox w-full -mt-20 bg-black">
+<div class="gridBox w-full bg-black">
     <h2 class="md:text-xl text-sm text-white font-bold">Powered by <span class="text-green-600 md:text-xl text-sm">Supabase</span> & <span class="text-green-600 md:text-xl text-sm">Nuxt 3</span></h2>
 </div>
 </template>
